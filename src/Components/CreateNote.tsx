5e0fdde968e1a06e54a3ab9721c7fa15f2c073bd
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-05-14 07:04:15 -0700

    form validation at frontend

--- src/Components/CreateNote.tsx
+++ src/Components/CreateNote.tsx
@@ -4,47 +4,63 @@ import { Form, Alert } from 'react-bootstrap'
 import { Note } from '../Models/NoteModel'
 import axios from 'axios'
 
-interface Props {
+interface FormValues {
     // notes: Note[],
     // setnotes: React.Dispatch<React.SetStateAction<Note[]>>,
-    title: String;
-    text: String;
-    color: String;
+    title: string;
+    text: string;
+    color: string;
 }
 
 export const CreateNote = () => {
-    const [error, setError] = useState<string>("");
+    // const [error, setError] = useState<string>("");
     // const titleRef = useRef<HTMLInputElement | null>(null);
     // const textRef = useRef<HTMLTextAreaElement | null>(null);
     // const ColorRef = useRef<HTMLInputElement | null>(null);
     const [data, setdata] = useState<string>("");
-    const [title, setTitle] = useState('');
-    const [text, setText] = useState("");
-    const [color, setColor] = useState("");
-    // const [post, setpost] = useState({
-    //     title: "",
-    //     text: "",
-    //     color: "",
-    //     date: "",
+    const [formValues, setFormValues] = useState<FormValues>({
+        title: "",
+        text: "",
+        color: ""
 
-    // });
+    });
+    const [error, setError] = useState<Partial<FormValues>>({});
 
+    // Handle form input
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {title, text} = e.target;
+        setFormValues((prevValues) => ({...prevValues, [title]: value }));
+    };
+    // Handle form submission
     const handleSubmit = async (e:FormEvent) =>{
         e.preventDefault();
-        try {
-            const newNote: Props = {
-                title,
-                text,
-                color,
-            };
-            const response = await axios.post('http://localhost:8080/send',newNote);
-            console.log("Note Entered:", response.data);
-            // Clear form input
-            setTitle("");
-            setText("");
-        } catch (error) {
-            console.error("Failed to create: ", error);
+        if (validateForm()) {
+            try {
+                const response = await axios.post('http://localhost:8080/send',formValues);
+                console.log("Note Entered:", response.data);
+                // Clear form input
+                // setFormValues({""});
+            } catch (error) {
+                console.error("Failed to create: ", error);
+            }
+        } else {
+            console.log("Form is invalide:", error);
+            return error
+
         }
+        
+    };
+
+    const validateForm = (): boolean => {
+        const newError: Partial<FormValues> = {};
+
+        if(!formValues.title) {
+            newError.title = 'Title Required'
+        } else if (!formValues.text) {
+            newError.text = 'Text Required'
+        } 
+        setError(newError);
+        return Object.keys(newError).length === 0;
     };
     // useEffect(() => {
     //     axios.get('http://localhost:8080')
@@ -63,18 +79,19 @@ export const CreateNote = () => {
         <div>{data}</div>
         <h2>Create Note</h2>
         <Form className='mt-3 mb-3' onSubmit={handleSubmit}>
-            {error && <Alert variant='danger'>{error}</Alert>}
             <Form.Group className='mb-3' controlId='formBasicTitle'>
                 <Form.Label>Title</Form.Label>
-                <Form.Control type='text' placeholder='Enter title for the note' onChange={(e) => setTitle(e.target.value)}/>
+                <Form.Control type='text' placeholder='Enter title for the note' name='title' value={formValues.title} onChange={handleInputChange}/>
+            {error.text && <Alert variant='danger'>{error.text}</Alert>}
             </Form.Group>
             <Form.Group className='mb-3' controlId='formBasicText'>
                 <Form.Label>Text</Form.Label>
-                <Form.Control placeholder='Enter Your note' as="textarea" rows={3} onChange={(e) => setText(e.target.value)}/>
+                <Form.Control placeholder='Enter Your note' as="textarea" rows={3} name='text' value={formValues.text} onChange={handleInputChange}/>
+                {error.text && <Alert variant='danger'>{error.text}</Alert>}
             </Form.Group>
             <Form.Group className='mb-3'>
                 <Form.Label htmlFor='colorInput'>Notes color</Form.Label>
-                <Form.Control type='color' id='colorInput' defaultValue="#dfdfdf" title='Choose your color' onChange={(e) => setColor(e.target.value)} />
+                <Form.Control type='color' id='colorInput' defaultValue="#dfdfdf" title='Choose your color' name='color' onChange={handleInputChange} />
             </Form.Group>
             <Button type="submit" variant='primary'> Submit</Button>
         </Form>
